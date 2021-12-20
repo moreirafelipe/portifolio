ea00d71c4b5b55b907be45780ab7ac0028426afc
FlipeFrontDev <felipe.saint@gmail.com>
2021-12-20 13:42:29 -0300

    Set menu logo icon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { ReactComponent as MenuSVG } from '../../assets/icons/menu.svg'
 import { Nav, Navbar, Container } from 'react-bootstrap';
 import './styles.scss'
 
@@ -20,14 +21,14 @@ export default function Header() {
   return (
     <Navbar className={colorChange ? 'navbar colorChange' : 'navbar'} collapseOnSelect expand="lg" variant={colorChange ? 'dark' : 'light'} >
       <Container>
-        <Navbar.Brand href="#start" className={colorChange ? 'text-info' : null}>F<span className={colorChange ? 'text-white' : null}>(m)</span></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand href="#start" className="bg-black text-info px-2">F<span className="bg-black text-white">(m)</span></Navbar.Brand>
+        <Navbar.Toggle className="navbar-toggle" aria-controls="responsive-navbar-nav" ><MenuSVG id="mob-icon" /></Navbar.Toggle >
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto fw-bold fs-5">
-            <Nav.Link href="#start">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#habilities">Habilities</Nav.Link>
-            <Nav.Link href="#about">Contact</Nav.Link>
+          <Nav className="menu-list mx-2 fw-bold fs-5">
+            <Nav.Link className="bg-black text-light menu-item m-1 px-1" href="#start">Home</Nav.Link>
+            <Nav.Link className="bg-black text-light menu-item m-1 px-1" href="#projects">Projects</Nav.Link>
+            <Nav.Link className="bg-black text-light menu-item m-1 px-1" href="#habilities">Habilities</Nav.Link>
+            <Nav.Link className="bg-black text-light menu-item m-1 px-1" href="#about">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
