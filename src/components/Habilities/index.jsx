68e9f947f03a726a935b4c6d13f0780aa1455d1b
FlipeFrontDev <felipe.saint@gmail.com>
2021-12-22 16:32:36 -0300

    Set english terms

--- src/components/Habilities/index.jsx
+++ src/components/Habilities/index.jsx
@@ -19,8 +19,8 @@ import './styles.scss'
 export function Habilities() {
   return (
     <div id="habilities" className="d-flex flex-column flex-wrap justify-content-center text-center align-content-center text-white bg-black mt-5 p-5">
-      <h3 className="w-100 text-center mt-5">Habilities</h3>
-      <p>There are some skills that I've been worked with recently:</p>
+      <h3 className="w-100 text-center mt-5">Hard Skills</h3>
+      <p>There are some skills that I've been working with recently:</p>
       <div className="container-icons d-flex flex-wrap flex-row justify-content-around align-items-center py-3">
         <BootstrapSVG className="skillIcon" />
         <ReactSVG className="skillIcon" />
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -39,7 +39,7 @@ export default function Header() {
           <Nav className="menu-list mt-2 fw-bold fs-5">
             <Nav.Link eventKey="1" as={Link} smooth to={"/#start"} className="bg-black text-light menu-item m-1 px-1" variant="black" ><span>Home</span></Nav.Link>
             <Nav.Link eventKey="2" as={Link} smooth to={"/#projects"} className="bg-black text-light menu-item m-1 px-1" ><span>Projects</span></Nav.Link>
-            <Nav.Link eventKey="3" as={Link} smooth to={"/#habilities"} className="bg-black text-light menu-item m-1 px-1" ><span>Habilities</span></Nav.Link>
+            <Nav.Link eventKey="3" as={Link} smooth to={"/#habilities"} className="bg-black text-light menu-item m-1 px-1" ><span>Hard Skills</span></Nav.Link>
             <Nav.Link eventKey="4" as={Link} smooth to={"/#footer"} className="bg-black text-light menu-item m-1 px-1" ><span>Contact</span></Nav.Link>
           </Nav>
         </Navbar.Collapse>
